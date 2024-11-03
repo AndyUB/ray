35ed4a85c37a9e672a95ee00db1ee3530d7065b9
Yuhan Ruan <andyubryh@gmail.com>
2024-11-03 01:40:09 -0700

    chore: Minimize minimal script
    Signed-off-by: Yuhan Ruan <andyubryh@gmail.com>

--- python/ray/dag/tests/experimental/abc_issue/abc_issue_min.py
+++ python/ray/dag/tests/experimental/abc_issue/abc_issue_min.py
@@ -1,7 +1,7 @@
 import torch
 import ray
 from ray.experimental.collective import allreduce
-from ray.dag import InputNode, MultiOutputNode
+from ray.dag import InputNode
 from typing import Tuple
 from ray.air._internal import torch_utils
 
@@ -24,12 +24,4 @@ actor = Worker.options(num_gpus=1).remote()
 # [TODO] Failed: No actor handle for class method output
 with InputNode() as inp:
     tensor1, tensor2 = actor.return_two.bind(inp)
-    tensor1 = allreduce.bind([tensor1])[0]
-    dag = MultiOutputNode([tensor1, tensor2])
-compiled_dag = dag.experimental_compile()
-ref = compiled_dag.execute(10)
-result = ray.get(ref)
-print(result)
-compiled_dag.teardown()
-
-ray.shutdown()
+    allreduce.bind([tensor1])
